a8dcb3adb9cebf9a10eba8c60b2ff1c624eb6f3f
Quan Dat Ho <hoquandat1247@gmail.com>
2023-10-31 19:57:23 +0700

    Test get a post with number of like

--- app/controllers/post.controllers.js
+++ app/controllers/post.controllers.js
@@ -1,4 +1,5 @@
-const { sequelize, Post } = require("../../models");
+const { sequelize, Post, Like } = require("../../models");
+const { Op } = require("sequelize");
 
 const getAllPosts = async (req, res) => {
 	const { user } = req;
@@ -12,29 +13,44 @@ const getAllPosts = async (req, res) => {
 
 const getPosts = async (req, res) => {
 	let arrPost = []
+	let arrPosts = [];
 	try {
 		const [result1] = await sequelize.query(`
-			SELECT Posts.id AS PostID, Posts.content AS content, Posts.userID AS Author_Id, Posts.fileUpload AS FileUpload, Posts.viewMode AS ViewMode, 
-			Posts.createdAt, Posts.updatedAt FROM Posts
+			SELECT Posts.id AS postID, Posts.content AS content, Posts.userID AS Author_Id, Posts.fileUpload AS FileUpload,
+			 Posts.viewMode AS ViewMode, Posts.createdAt, Posts.updatedAt FROM Posts
 			INNER JOIN Friends AS friend
 			ON (friend.userID = "1" && friend.friendID = Posts.userID && friend.isFriend = '1' AND Posts.viewMode = "Friend") 
 			|| (friend.friendID = "1" && friend.userID = Posts.userID && friend.isFriend = '1' AND Posts.viewMode = "Friend");
 		`);
 
 		const [result2] = await sequelize.query(`
-			SELECT  Posts.id AS PostID, Posts.content AS content, Posts.userID AS Author_Id, Posts.fileUpload AS FileUpload, Posts.viewMode AS ViewMode, 
+			SELECT  Posts.id AS postID, Posts.content AS content, Posts.userID AS Author_Id, Posts.fileUpload AS FileUpload, Posts.viewMode AS ViewMode, 
 			Posts.createdAt, Posts.updatedAt FROM Posts
 			WHERE (Posts.viewMode = 'Everyone') 
 			|| (Posts.viewMode = 'Only me' && Posts.userID = '1') 
 			|| (Posts.viewMode = 'Friend' && Posts.userID = '1');
 		`);
 
-		arrPost = [...result1, ...result2]
-		arrPost.sort((a,b) => {
+		arrPost = [...result1, ...result2].sort((a,b) => {
 			return b.createdAt - a.createdAt;
 		})
 
-		res.status(200).send(arrPost)
+
+
+		arrPost.forEach(async (item, index) => {
+			const numberOfLike = await Like.count({
+				where: {
+					postID: item.postID
+				}
+			})
+
+			arrPosts.push({...item, numberOfLike})
+		})
+
+		// console.log({arrPosts})
+
+
+		res.status(200).send(arrPosts)
 	} catch(err) {
 		res.status(500).send(err);
 	}
